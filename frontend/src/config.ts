cd5c4934199e6fe58e92f717508fe21a26b94ad8
LearnRK <learn.rk10101@gmail.com>
2024-08-04 15:13:33 +0530

    deployed backend on render and updated the backend api route (render)

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://57845aef-505b-40a3-9179-1e48526405db-00-dp6axrnklxf9.sisko.replit.dev/api/v1";
-export const APPOINT_URL = "https://57845aef-505b-40a3-9179-1e48526405db-00-dp6axrnklxf9.sisko.replit.dev/appointme";
+export const BACKEND_URL = "https://bc-backend-4dh0.onrender.com/api/v1";
+export const APPOINT_URL = "https://bc-backend-4dh0.onrender.com/appointme";
